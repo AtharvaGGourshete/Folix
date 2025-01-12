7ef53182c44b8a82acf7cc2dcb5adf7a6ad92f1b
AtharvaGGourshete <goursheteatharva@gmail.com>
2025-01-12 15:25:47 +0530

    Skills, education section updated, figured out file routing

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,32 +16,7 @@ function Nav() {
   const [search, setSearch] = useSearchParams();
   const [showSignIn, setShowSignIn] = useState(false);
   const { user } = useUser();
-  // const content = (
-  //   <>
-  //     <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-950 transition">
-  //       <ul className="text-center text-xl p-20">
-  //         <Link spy={true} smooth={true} to="Home">
-  //           <li className="my-4 py-4 border-b bg-slate-800 hover:bg-slate-800 hover:rounded">
-  //             Home
-  //           </li>
-  //         </Link>
-  //         <Link spy={true} smooth={true} to="Education">
-  //           <li className="my-4 py-4 border-b bg-slate-800 hover:bg-slate-800 hover:rounded">
-  //             Education
-  //           </li>
-  //         </Link>
-  //         <Link
-  //           spy={true}
-  //           smooth={true}
-  //           to="/login"
-  //           className="my-4 py-4 border-b bg-slate-800 hover:bg-slate-800 hover:rounded"
-  //         >
-  //           <li>Log In</li>
-  //         </Link>
-  //       </ul>
-  //     </div>
-  //   </>
-  // );
+
   useEffect(() => {
     if (search.get("sign-in")) {
       setShowSignIn(true);
@@ -56,32 +31,23 @@ function Nav() {
   };
   return (
     <>
-      <nav className="bg-slate-950">
+      <nav className="bg-slate-950 ">
         <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4">
-          <div className="flex items-center flex-1">
+          <div className="flex items-center flex-1 justify-around">
             <span className="text-3xl font-bold">Folix</span>
           </div>
-          <div className="lg:flex md:flex lg:flex-1 items-center justify-end font-normal hidden">
+          <div className="lg:flex md:flex lg:flex-1 items-center justify-evenly font-normal hidden">
             <div className="flex-10">
               <ul className="flex gap-8 mr-16 text-[18px]">
-                {/* <Link spy={true} smooth={true} to="Home">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-950 hover:border-fuchsia-600 cursor-pointer">
-                  Home
-                </li>
-              </Link>
-              <Link spy={true} smooth={true} to="Education">
-                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-950 hover:border-fuchsia-600 cursor-pointer">
-                  Education
-                </li>
-              </Link> */}
+                
                 <SignedOut>
                   <Link to="/">
                     <Button variant="ghost">Home</Button>
                   </Link>
 
-                  <Link to="/education">
+                  {/* <Link to="/education">
                     <Button variant="ghost">Education</Button>
-                  </Link>
+                  </Link> */}
 
                   <Button
                     variant="destructive"
@@ -91,13 +57,13 @@ function Nav() {
                   </Button>
                 </SignedOut>
                 <SignedIn>
-                  <Link to="/">
+                  {/* <Link to="/">
                     <Button variant="ghost">Home</Button>
                   </Link>
 
                   <Link to="/education">
                     <Button variant="ghost">Education</Button>
-                  </Link>
+                  </Link> */}
                   <UserButton
                     appearance={{
                       elements: {
---
+++ src/images/Images.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Images = {
+    image1 : new URL('./HTML.png', import.meta.url).href,
+    image2 : new URL('./CSS.png', import.meta.url).href,
+    image3 : new URL('./JS.png', import.meta.url).href,
+    image4 : new URL('./React.png', import.meta.url).href,
+    image5 : new URL('./JS.png', import.meta.url).href,
+    image6 : new URL('./JS.png', import.meta.url).href
+}
+
+export default Images
